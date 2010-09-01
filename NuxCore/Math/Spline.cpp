0eae87d4e4945a2110972cce15b6fcc44a0d31eb
Neil Jagdish Patel <neil.patel@canonical.com>
2010-09-01 16:18:37 -0400

    Add licensing
    (bzr r5)

--- NuxCore/Math/Spline.cpp
+++ NuxCore/Math/Spline.cpp
@@ -1,3 +1,25 @@
+/*
+ * Copyright 2010 Inalogic Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify it 
+ * under the terms of the GNU Lesser General Public License version 3, as
+ * published by the  Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but 
+ * WITHOUT ANY WARRANTY; without even the implied warranties of 
+ * MERCHANTABILITY, SATISFACTORY QUALITY or FITNESS FOR A PARTICULAR 
+ * PURPOSE.  See the applicable version of the GNU Lesser General Public 
+ * License for more details.
+ * 
+ * You should have received a copy of both the GNU Lesser General Public 
+ * License version 3 along with this program.  If not, see 
+ * <http://www.gnu.org/licenses/>
+ *
+ * Authored by: Jay Taoko <jay.taoko_AT_gmail_DOT_com>
+ *
+ */
+
+
 #include "../NKernel.h"
 #include "Spline.h"
 #include "MathFunctions.h"
