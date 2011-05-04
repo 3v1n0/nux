15b26aae96dcf0b9abe2e1154838fd7e712b725c
Tim Penhey <tim.penhey@canonical.com>
2011-05-04 11:59:32 +0200

    Point2D done.
    (bzr r344.2.2)

---
+++ NuxCore/Math/Point2D-inl.h
@@ -0,0 +1,34 @@
+#ifndef POINT2D_INL_H
+#define POINT2D_INL_H
+
+namespace nux
+{
+
+  template<typename T>
+  Point2D<T>::Point2D()
+      : x(0), y(0)
+  {
+  }
+
+  template<typename T>
+  Point2D<T>::Point2D(T const& X, T const& Y)
+      : x(X), y(Y)
+  {
+  }
+
+  template<typename T>
+  bool operator ==(const Point2D<T>& lhs, const Point2D<T>& rhs)
+  {
+      return (lhs.x == rhs.x) && (lhs.y == rhs.y);
+  }
+
+  template<typename T>
+  bool operator !=(const Point2D<T>& lhs, const Point2D<T>& rhs);
+  {
+    return !(lhs == rhs);
+  }
+
+}
+
+
+#endif
