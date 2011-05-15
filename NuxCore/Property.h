65acfb7a7ce1b85265b101b509e489a269d092d3
Tim Penhey <tim.penhey@canonical.com>
2011-05-16 10:35:23 +1200

    Add property work from sprint spike.
    (bzr r345.1.1)

---
+++ NuxCore/Property.h
@@ -0,0 +1,180 @@
+#ifndef NUXCORE_PROPERTY_H
+#define NUXCORE_PROPERTY_H
+
+#include "PropertyTraits.h"
+
+#include <map>
+#include <sigc++/signal.h>
+
+
+namespace nux {
+
+// TODO:
+//  range type trait checks
+//  object serialisation
+
+template <typename VALUE_TYPE,
+          template <typename> class TRAITS = type::assignable>
+class connectable_property
+{
+public:
+  typedef TRAITS<VALUE_TYPE> TraitsType;
+
+  connectable_property()
+  : value_(VALUE_TYPE())
+  , notify_(true)
+  {}
+
+  connectable_property(VALUE_TYPE const& initial)
+  : value_(initial)
+  , notify_(true)
+  {}
+
+  template <typename T2>
+  VALUE_TYPE& operator=(const T2& value)
+  {
+      bool is_changed = false;
+      if (value != value_) {
+        is_changed = TraitsType::assign(value_, value);
+      }
+      if (is_changed && notify_) {
+        changed.emit(value_);
+      }
+      return value_;
+  }
+
+  operator VALUE_TYPE const & () const {
+    return value_;
+  }
+
+  VALUE_TYPE const& value() const
+  {
+    return value_;
+  }
+
+  sigc::signal<void, VALUE_TYPE const&> changed;
+
+  void freeze()
+  {
+    notify_ = false;
+  }
+
+  void thaw()
+  {
+    notify_ = true;
+  }
+
+private:
+  VALUE_TYPE value_;
+  bool notify_;
+};
+
+
+
+
+class property_base
+{
+public:
+  virtual bool set_value(std::string const& serialized_form) = 0;
+  virtual std::string get_serialized_value() = 0;
+};
+
+
+class introspectable
+{
+public:
+  // Needs to have a container of properties
+
+  /// If the property was not able to be set with the value, the method
+  /// returns false.
+  template <typename T>
+  bool set_property(std::string const& name, T const& value)
+    {
+      // TODO: use boost::serialisation for a binary format?
+      // TODO: use an iterator to check the property exists.
+      // find the property and set the value...
+      // make this nicer
+      return properties_[name]->set_value(
+          type::serializable<T>::to_string(value));
+   }
+
+  template <typename T>
+  T get_property(std::string const& name, T* foo = 0)
+    {
+      std::string s = properties_[name]->get_serialized_value();
+      std::pair<T, bool> result = type::serializable<T>::from_string(s);
+      // assert(result.second); -- should never fail, but we don't want an assert.
+      return result.first;
+    }
+
+  void add_property(std::string const& name, property_base* property)
+    {
+      // check to see if it exists and if it does barf horribly as we can't
+      // have two properties with the same name;
+      properties_[name] = property;
+    }
+
+private:
+  typedef std::map<std::string, property_base*> PropertyContainer;
+  PropertyContainer properties_;
+};
+
+
+
+
+template <typename T, typename TRAITS = type::assignable<T> >
+class property : public property_base
+{
+public:
+  property(introspectable* owner, std::string const& name) : name_(name), value_(T())
+    {
+      owner->add_property(name, this);
+    }
+  property(introspectable* owner, std::string const& name, T const& initial) : name_(name), value_(initial)
+    {
+      owner->add_property(name, this);
+    }
+
+  T & operator = (const T &i) {
+    return value_ = i;
+  }
+
+  // Think about a traits type that is used for assignment to allow
+  // the specialisation for std::string to get assignment from char*
+  // but disallow the general case.
+
+  // This template class member function template serves the purpose to make
+  // typing more strict. Assignment to this is only possible with exact identical
+  // types.
+  template <typename T2> T2 & operator = (const T2 &i) {
+    T2 &guard = value_;
+    throw guard; // Never reached.
+  }
+
+  operator T const & () const {
+    return value_;
+  }
+
+  virtual bool set_value(std::string const& serialized_form)
+    {
+        std::pair<T, bool> result = TRAITS::from_string(serialized_form);
+        if (result.second) {
+          value_ = result.first;
+        }
+        return result.second;
+    }
+  virtual std::string get_serialized_value()
+    {
+      return TRAITS::to_string(value_);
+    }
+
+private:
+  std::string name_;
+  T value_;
+};
+
+
+
+}
+
+#endif
---
+++ NuxCore/PropertyTraits.h
@@ -0,0 +1,206 @@
+#ifndef NUXCORE_PROPERTY_TRAITS_H
+#define NUXCORE_PROPERTY_TRAITS_H
+
+#include <string>
+#include <boost/lexical_cast.hpp>
+
+namespace nux {
+namespace type {
+
+/**
+ * Here we want to have only explict type support, to avoid someone using the
+ * system with something we haven't though of and getting upset when if it
+ * doesn't work properly.
+ */
+
+template <typename T>
+struct assignable
+{
+    /**
+     * Expected content:
+     *
+     * static bool assign(int& var, int new_value)
+     */
+};
+
+template <typename T>
+struct serializable
+{
+    /**
+     * Expected content:
+     *
+     * static std::string to_string(T const& value)
+     *  - used to convert the type to a string
+     *
+     * static std::pair<T, bool> from_string(std::string const& serialized_form)
+     *  - If the serialized form is convertable to T, then we get:
+     *      (value, true)
+     *  - If it is not convertable we get:
+     *      (T(), false)
+     */
+};
+
+
+/**
+ * The serializable_impl template has different signatures than the standard
+ * serializable traits class to avoid the generalised template being used as a
+ * traits type when we really don't want it being used in that way, but we do
+ * want a generalized implementation for the types we do care about.
+ */
+template <typename T>
+struct serializable_impl
+{
+  static std::string to_string_impl(T const& value)
+  {
+    return boost::lexical_cast<std::string>(value);
+  }
+
+  static std::pair<T, bool> from_string_impl(std::string const& serialized_form)
+  {
+    try {
+      return std::make_pair<T, bool>(
+          boost::lexical_cast<T>(serialized_form), true);
+    }
+    catch (boost::bad_lexical_cast const& e) {
+      return std::make_pair<T, bool>(T(), false);
+    }
+  }
+
+};
+
+
+template <>
+struct serializable<int>
+{
+  static std::string to_string(int value)
+  {
+      return serializable_impl<int>::to_string_impl(value);
+  }
+
+  static std::pair<int, bool> from_string(std::string const& serialized_form)
+  {
+      return serializable_impl<int>::from_string_impl(serialized_form);
+  }
+};
+
+template <>
+struct assignable<int> : serializable<int>
+{
+  static bool assign(int& var, int new_value)
+  {
+    var = new_value;
+    return true;
+  }
+};
+
+
+
+template <>
+struct serializable<float>
+{
+  static std::string to_string(float value)
+  {
+      return serializable_impl<float>::to_string_impl(value);
+  }
+
+  static std::pair<float, bool> from_string(std::string const& serialized_form)
+  {
+      return serializable_impl<float>::from_string_impl(serialized_form);
+  }
+};
+
+template <>
+struct assignable<float> : serializable<float>
+{
+  static bool assign(float& var, float new_value)
+  {
+    var = new_value;
+    return true;
+  }
+};
+
+
+template <>
+struct serializable<double>
+{
+  static std::string to_string(double value)
+  {
+      return serializable_impl<double>::to_string_impl(value);
+  }
+
+  static std::pair<double, bool> from_string(std::string const& serialized_form)
+  {
+      return serializable_impl<double>::from_string_impl(serialized_form);
+  }
+};
+
+template <>
+struct assignable<double> : serializable<double>
+{
+  static bool assign(double& var, double new_value)
+  {
+    var = new_value;
+    return true;
+  }
+};
+
+
+template <>
+struct serializable<bool>
+{
+  static std::string to_string(bool value)
+  {
+      return value ? "true" : "false";
+  }
+
+  static std::pair<bool, bool> from_string(std::string const& serialized_form)
+  {
+    if (serialized_form == "true") {
+      return std::make_pair(true, true);
+    } else if (serialized_form == "false") {
+      return std::make_pair(false, true);
+    } else {
+      return std::make_pair(false, false);
+    }
+  }
+};
+
+template <>
+struct assignable<bool> : serializable<bool>
+{
+  static bool assign(bool& var, bool new_value)
+  {
+    var = new_value;
+    return true;
+  }
+};
+
+
+template <>
+struct serializable<std::string>
+{
+  static std::string to_string(std::string const& value)
+    {
+      return value;
+    }
+
+  static std::pair<std::string, bool> from_string(std::string const& serialized_form)
+    {
+      return std::make_pair(serialized_form, true);
+    }
+};
+
+template <>
+struct assignable<std::string> : serializable<std::string>
+{
+  static bool assign(std::string& var, std::string const& new_value)
+  {
+    var = new_value;
+    return true;
+  }
+};
+
+}
+}
+
+#endif
