8f14c244a238753923e8961e4a76c06b066a5d70
Tim Penhey <tim.penhey@canonical.com>
2011-06-03 13:19:56 +1200

    Initial logger test case.
    (bzr r356.3.1)

---
+++ tests/test_logger.cpp
@@ -0,0 +1,21 @@
+#include "NuxCore/Logger.h"
+
+#include <gtest/gtest.h>
+
+
+using nux::logging;
+
+namespace {
+
+TEST(TestLogger, TestDefaultConstruction) {
+
+  Logger logger;
+
+  EXPECT_TRUE(logger.IsErrorEnabled());
+  EXPECT_TRUE(logger.IsWarningEnabled());
+  EXPECT_FALSE(logger.IsInfoEnabled());
+  EXPECT_FALSE(logger.IsDebugEnabled());
+
+}
+
+}
