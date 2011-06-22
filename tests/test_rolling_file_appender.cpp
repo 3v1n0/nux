9aaa6415902e6a302f8198887612646f12a264c7
Tim Penhey <tim.penhey@canonical.com>
2011-06-22 13:44:29 +1200

    More tests.
    (bzr r356.4.14)

--- tests/test_rolling_file_appender.cpp
+++ tests/test_rolling_file_appender.cpp
@@ -2,53 +2,90 @@
 #include <gmock/gmock.h>
 
 #include <fstream>
+#include <streambuf>
+#include <string>
 
 #include <boost/filesystem.hpp>
 #include <boost/filesystem/fstream.hpp>
 
-// using namespace nux::logging;
+#include "NuxCore/RollingFileAppender.h"
+
+namespace bf = boost::filesystem;
+
+using namespace nux::logging;
 using namespace testing;
 
 namespace {
 
+const std::string TEST_ROOT("/tmp/nux-test-cases");
+
+TEST(TestRootDir, NoTestRoot) {
+  // The test root should not exist.
+  EXPECT_FALSE(bf::exists(TEST_ROOT));
+}
+
 class TestRollingFileAppender : public ::testing::Test
 {
-public:
-  TestRollingFileAppender()
-    : test_root("/tmp/nux-test-cases")
-    {
-    }
-
- protected:
-  boost::filesystem::path test_root;
-
+protected:
   virtual void SetUp() {
-    // Make the unity test directory in /tmp
-    boost::filesystem::create_directories(test_root);
+    // Make sure that the tests start with nothing there.
+    bf::remove_all(TEST_ROOT);
   }
 
- virtual void TearDown() {
-   // Delete the unity test directory
-   boost::filesystem::remove_all(test_root);
- }
+  virtual void TearDown() {
+    // Delete the unity test directory
+    bf::remove_all(TEST_ROOT);
+  }
+
+  std::string ReadFile(std::string const& filename) {
+    std::ifstream input(filename.c_str());
+    return std::string((std::istreambuf_iterator<char>(input)),
+                       std::istreambuf_iterator<char>());
+  }
 };
-
-
-TEST_F(TestRollingFileAppender, TestSetup) {
-
-  boost::filesystem::ofstream fout(test_root / "testing");
-  fout << "Testing";
-  fout.close();
-
-  EXPECT_TRUE(boost::filesystem::exists("/tmp/nux-test-cases/testing"));
-}
 
 TEST_F(TestRollingFileAppender, TestCreatesFile) {
 
-  nux::logging::RollingFileAppender output(test_root, "nux");
+  std::string logfile = TEST_ROOT + "/nux.log";
+  RollingFileAppender output(logfile, 5, 1e6);
 
-  EXPECT_TRUE(boost::filesystem::exists(test_root / "nux"));
-  EXPECT_TRUE(boost::filesystem::is_regular_file(test_root / "nux"));
+  EXPECT_TRUE(bf::exists(logfile));
+  EXPECT_TRUE(bf::is_regular_file(logfile));
 }
+
+TEST_F(TestRollingFileAppender, TestNestedDirectories) {
+
+  std::string logfile = TEST_ROOT + "/nested/directories/nux.log";
+  RollingFileAppender output(logfile, 5, 1e6);
+
+  EXPECT_TRUE(bf::exists(logfile));
+  EXPECT_TRUE(bf::is_regular_file(logfile));
+}
+
+TEST_F(TestRollingFileAppender, TestWritesToLogFile) {
+
+  std::string logfile = TEST_ROOT + "/nux.log";
+  RollingFileAppender output(logfile, 5, 1e6);
+
+  output << "Testing writing" << std::flush;
+
+  EXPECT_THAT(ReadFile(logfile), Eq("Testing writing"));
+}
+
+TEST_F(TestRollingFileAppender, TestLogFileRolls) {
+
+  std::string logfile = TEST_ROOT + "/nux.log";
+  unsigned max_log_size = 20; // roll every 20 characters
+  RollingFileAppender output(logfile, 5, max_log_size);
+
+  output << "Testing the rolling of the logfile" << std::endl
+         << "Second line is in the primary logfile" << std::endl;
+
+  EXPECT_THAT(ReadFile(logfile),
+              Eq("Second line is in the primary logfile\n"));
+  EXPECT_THAT(ReadFile(logfile + ".1"),
+              Eq("Testing the rolling of the logfile\n"));
+}
+
 
 } // anon namespace
