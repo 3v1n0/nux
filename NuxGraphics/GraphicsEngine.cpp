111130e1b52bfac1465ce858eb0b17141dcf00ef
Jay Taoko <jay.taoko@canonical.com>
2011-09-20 20:50:47 -0400

    Rewrote some Fx functions to minimize texture creation
    (bzr r474.1.1)

--- NuxGraphics/GraphicsEngine.cpp
+++ NuxGraphics/GraphicsEngine.cpp
@@ -139,6 +139,17 @@ namespace nux
   }
 
 
+  FxStructure::FxStructure()
+  {
+    src_texture = nux::GetGraphicsDisplay()->GetGpuDevice()->CreateTexture(1, 1, 1, nux::BITFMT_R8G8B8A8);
+    dst_texture = nux::GetGraphicsDisplay()->GetGpuDevice()->CreateTexture(1, 1, 1, nux::BITFMT_R8G8B8A8);
+    temp_texture = nux::GetGraphicsDisplay()->GetGpuDevice()->CreateTexture(1, 1, 1, nux::BITFMT_R8G8B8A8);
+  }
+
+  FxStructure::~FxStructure()
+  {
+  }
+
   GraphicsEngine::GraphicsEngine (GraphicsDisplay &GlWindow, bool create_rendering_data)
     :   _graphics_display (GlWindow)
   {
