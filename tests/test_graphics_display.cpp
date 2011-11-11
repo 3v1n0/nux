2e915f9fff22069bef892aa9730d4ef50d016a5f
Tim Penhey <tim.penhey@canonical.com>
2011-11-11 13:40:50 +1300

    Make the a test-apps target in the test directory, and make it called as part of make check.
    (bzr r515.2.1)

--- tests/test_graphics_display.cpp
+++ tests/test_graphics_display.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright 2010 Inalogic Inc.
+ * Copyright 2011 Inalogic Inc.
  *
  * This program is free software: you can redistribute it and/or modify it
  * under the terms of the GNU General Public License version 3, as published
@@ -18,36 +18,40 @@
  *
  */
 
+#include <iostream>
+
 #include "Nux/Nux.h"
 #include "Nux/VLayout.h"
-#include "Nux/HLayout.h"
 #include "Nux/WindowThread.h"
-#include "Nux/TextEntry.h"
- 
+
 void ThreadWidgetInit(nux::NThread* thread, void* InitData)
 {
-    nux::VLayout* MainVLayout = new nux::VLayout(NUX_TRACKER_LOCATION);
-    
-    MainVLayout->SetVerticalInternalMargin(10);
-
-    MainVLayout->SetContentDistribution(nux::eStackCenter);
-    nux::GetWindowThread ()->SetLayout(MainVLayout);
-    nux::ColorLayer background(nux::Color(0xFF4D4D4D));
-    static_cast<nux::WindowThread*>(thread)->SetWindowBackgroundPaintLayer(&background);
+  nux::VLayout* layout = new nux::VLayout(NUX_TRACKER_LOCATION);
+  nux::GetWindowThread()->SetLayout(layout);
 }
 
 int main(int argc, char **argv)
 {
     nux::NuxInitialize(0);
-    nux::WindowThread* wt = nux::CreateGUIThread(TEXT("Text Entry"), 400, 300, 0, &ThreadWidgetInit, 0);
-   
+    // When the GUI thread is created, it creates the GraphicsDisplay.
+    nux::WindowThread* wt = nux::CreateGUIThread(TEXT("Text Entry"),
+                                                 400, 300, 0, &ThreadWidgetInit, 0);
+
     // Test to see if the variable in GraphicsDisplay is initialized
-    int is_initalized = 1;    
-    nux::GetGraphicsDisplay()->PointerIsGrabbed() == false ? is_initalized = 0 : is_initalized = 1;
- 
-    // Dont want to make the window pop up 
+    int result = 1;
+    if (!nux::GetGraphicsDisplay()->PointerIsGrabbed())
+    {
+      // Zero return code is good.
+      result = 0;
+    }
+    else
+    {
+      std::cerr << "FAIL: GraphicsDisplay hasn't correctly initialized.\n";
+    }
+
+    // Dont want to make the window pop up
     //wt->Run(NULL);
 
     delete wt;
-    return is_initalized;
+    return result;
 }
