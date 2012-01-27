7ce6f4717e78c95d5ecc09fd697ad6d074060bbe
Jay Taoko <jay.taoko@canonical.com>
2012-01-26 19:39:43 -0500

    * New TextEntry class (updated from google gadgets for linux: http://code.google.com/p/google-gadgets-for-linux/source/browse/trunk/extensions/gtk_edit_element/) * Added InputMethod code (although not connected) * Added TextEntry example (to be used for development testing)
    (bzr r544.9.1)

---
+++ Nux/InputMethodContext.h
@@ -0,0 +1,261 @@
+#ifndef INPUTMETHODCONTEXT_H
+#define INPUTMETHODCONTEXT_H
+
+
+namespace nux
+{
+  class IMEContext;
+  class KeyEvent;
+  class View;
+
+  class KeyEvent
+  {
+  public:
+
+    KeyEvent(NuxEventType type,
+      unsigned int key_code,
+      unsigned int mouse_state, unsigned int event_flags)
+      : type_(type)
+      , key_code_(key_code)
+      , key_modifiers_(event_flags)
+      , mouse_state_(mouse_state)
+    {
+    }
+
+    NuxEventType type() const {return type_;}
+    unsigned int key_code() const {return key_code_;}
+    unsigned int flags() const {return key_modifiers_;}
+    unsigned int MouseState() const {return mouse_state_;}
+
+
+    bool IsShiftDown() const { return (key_modifiers_ & KEY_MODIFIER_SHIFT) != 0; }
+    bool IsControlDown() const { return (key_modifiers_ & KEY_MODIFIER_CTRL) != 0; }
+    bool IsCapsLockDown() const { return (key_modifiers_ & KEY_MODIFIER_CAPS_LOCK) != 0; }
+    bool IsAltDown() const { return (key_modifiers_ & KEY_MODIFIER_ALT) != 0; }
+
+//     bool IsMouseEvent() const {
+//       return type_ == EVENT_MOUSE_DOWN ||
+//         type_ == EVENT_MOUSE_DRAGGED ||
+//         type_ == EVENT_MOUSE_UP ||
+//         type_ == EVENT_MOUSE_MOVE ||
+//         type_ == EVENT_MOUSE_ENTER ||
+//         type_ == EVENT_MOUSE_LEAVE ||
+//         type_ == EVENT_MOUSE_WHEEL;
+//     }
+
+  private:
+    EventType type_;
+    unsigned int key_code_;
+    unsigned int key_modifiers_;
+    unsigned int mouse_state_;
+
+    KeyEvent(const KeyEvent&);
+    void operator = (const KeyEvent&);
+  };
+
+  struct CompositionUnderline
+  {
+    CompositionUnderline()
+      : start_offset(0),
+      end_offset(0),
+      color(0),
+      thick(false) {}
+
+    CompositionUnderline(unsigned int s, unsigned int e, Color c, bool t)
+      : start_offset(s),
+      end_offset(e),
+      color(c),
+      thick(t) {}
+
+    unsigned int start_offset;
+    unsigned int end_offset;
+    Color color;
+    bool thick;
+  };
+
+  // WebKit only supports underline attribue for composition, so we use
+  // CompositionUnderline as CompositionAttribute right now.
+  typedef struct CompositionUnderline CompositionAttribute;
+  typedef std::vector<CompositionAttribute> CompositionAttributeList;
+
+  // CommitTextListener is notified when a text is commited from the context.
+  class CommitTextListener
+  {
+  public:
+    virtual void OnCommitText(IMEContext* sender,
+      const std::wstring& text) = 0;
+
+  protected:
+    virtual ~CommitTextListener() {}
+  };
+
+  // CompositionListener is notified when composition of the context is changed.
+  class CompositionListener
+  {
+  public:
+    virtual void OnStartComposition(IMEContext* sender) = 0;
+    virtual void OnEndComposition(IMEContext* sender) = 0;
+    virtual void OnSetComposition(IMEContext* sender,
+      const std::wstring& text,
+      const CompositionAttributeList& attributes,
+      unsigned int cursor_pos) = 0;
+
+  protected:
+    virtual ~CompositionListener() {}
+  };
+
+  // CompositionListener is notified when a key event is forwarded from
+  // the context.
+  class ForwardKeyEventListener
+  {
+  public:
+    virtual void OnForwardKeyEvent(IMEContext* sender,
+      const KeyEvent& event) = 0;
+
+  protected:
+    virtual ~ForwardKeyEventListener() {}
+  };
+
+  // SurroundingListener is notified when an input method context needs to
+  // manipulate the text surround the input cursor. If associated view supports
+  // surrounding, it should set the listener to input method context. Some input
+  // methods generate different result depends on the chars before or after the
+  // input cursor.
+  //
+  // For example:
+  // Some Korean input method:
+  //      Key events                  Unicode char
+  // 1    'C'                          +U314a
+  // 2    'K'                          +U314f
+  // 3    'C' 'K'                      +Ucc28
+  //
+  // In case 2 and 3, when users press 'K', the input method will check the char
+  // before input cursor. If the char is +U314a, it will remove the char and
+  // insert a new char +Ucc28. If no char before input cursor, it will insert char
+  // +U314f.
+  //
+  // See also OnSetSurroundingActive() and OnDeleteSurrounding().
+  class SurroundingListener
+  {
+  public:
+    // Activate or inactivate surrounding support. When surrounding is activated,
+    // The assoicated view should call SetSurrounding() to notify any changes of
+    // text surround the input cursor.
+    // Return true if associated view can support surrounding.
+    virtual bool OnSetSurroundingActive(IMEContext* sender,
+      bool activate) = 0;
+
+    // Delete a picse of text surround the input cursor.
+    // Return true if associated view delete the surrounding text successfully.
+    virtual bool OnDeleteSurrounding(IMEContext* sender,
+      int offset,
+      int chars) = 0;
+  protected:
+    virtual ~SurroundingListener() {}
+  };
+
+  class IMEContext
+  {
+  public:
+    virtual ~IMEContext() {}
+
+    // Set associated view.
+    void set_view(View* view) { view_ = view; }
+
+    // Get associated view.
+    const View* view() const { return view_; }
+
+    // Set a listener to receive a callback when im context commits a text.
+    void set_commit_text_listener(CommitTextListener* listener)
+    {
+      commit_text_listener_ = listener;
+    }
+
+    // Set a listener to receive a callback when im context changes composition.
+    void set_composition_listener(CompositionListener* listener)
+    {
+      composition_listener_ = listener;
+    }
+
+    // Set a listener to receive a callback when im context forwards a key event.
+    void set_forward_key_event_listener(ForwardKeyEventListener* listener)
+    {
+      forward_key_event_listener_ = listener;
+    }
+
+    // Set a listener to receive a callback when im context need operater
+    // surrounding.
+    void set_surrounding_listener(SurroundingListener* listener)
+    {
+      surrounding_listener_ = listener;
+    }
+
+    // Tell the context it got/lost focus.
+    virtual void Focus() = 0;
+    virtual void Blur() = 0;
+
+    // Reset context, context will be reset to initial state.
+    virtual void Reset()  = 0;
+
+    // Filter key event, returns true, if the key event is handled,
+    // associated widget should ignore it.
+    virtual bool FilterKeyEvent(const KeyEvent& event) = 0;
+
+    // Set text input cursor location on screen.
+    virtual void SetCursorLocation(const Rect& caret_rect) = 0;
+
+    // Set surrounding context.
+    virtual void SetSurrounding(const std::wstring& text,
+      int cursor_pos) = 0;
+
+    // Create an im context.
+    static IMEContext* Create(View* view);
+
+  protected:
+    explicit IMEContext(View* view);
+
+    // Commit text.
+    void CommitText(const std::wstring& text);
+
+    // Start compostion.
+    void StartComposition();
+
+    // End composition.
+    void EndComposition();
+
+    // Set composition.
+    void SetComposition(const std::wstring& text,
+      const CompositionAttributeList& attributes,
+      unsigned int cursor_pos);
+
+    // Forward key event.
+    void ForwardKeyEvent(const KeyEvent& event);
+
+    // Notify the associated view whether or not the input context needs
+    // surrounding. When surrounding is activated, associated view should
+    // call SetSurrounding() to update any changes of text arround the input
+    // cursor.
+    // Return true if associated view can support surrounding.
+    bool SetSurroundingActive(bool activate);
+
+    // Delete surrouding arround cursor. Return true, if it is handled.
+    bool DeleteSurrounding(int offset, int nchars);
+
+  private:
+    // Client view.
+    View* view_;
+
+    // Listeners:
+    CommitTextListener* commit_text_listener_;
+    CompositionListener* composition_listener_;
+    ForwardKeyEventListener* forward_key_event_listener_;
+    SurroundingListener* surrounding_listener_;
+
+    IMEContext(const IMEContext&);
+    void operator = (const IMEContext&);
+  };
+
+}
+
+#endif  // INPUTMETHODCONTEXT_H
+
---
+++ Nux/InputMethodIBus.cpp
@@ -0,0 +1,323 @@
+#include "Nux.h"
+#include <ibus.h>
+
+#include "InputMethodIBus.h"
+
+// #include "base/logging.h"
+// #include "base/utf_string_conversions.h"
+// #include "third_party/skia/include/core/SkColor.h"
+// #include "ui/base/gtk/gtk_signal.h"
+// #include "ui/base/keycodes/keyboard_code_conversion_gtk.h"
+// #include "ui/gfx/rect.h"
+// #include "views/events/event.h"
+// #include "views/ime/ime_context.h"
+
+namespace nux
+{
+  IBusBus* IBusIMEContext::bus_ = NULL;
+
+  IBusIMEContext::IBusIMEContext(View* view)
+    : IMEContext(view),
+    context_(NULL),
+    is_focused_(false)
+  {
+    // init ibus
+    if (!bus_)
+    {
+      ibus_init();
+      bus_ = ibus_bus_new();
+    }
+
+    // connect bus signals
+    g_signal_connect(bus_, "connected",    G_CALLBACK(OnConnected_),     this);
+    g_signal_connect(bus_, "disconnected", G_CALLBACK(OnDisconnected_),  this);
+
+    if (ibus_bus_is_connected(bus_))
+    {
+      CreateContext();
+    }
+    else
+    {
+      nuxDebugMsg("[IBusIMEContext::IBusIMEContext] Can not connect to ibus");
+    }
+  }
+
+  IBusIMEContext::~IBusIMEContext()
+  {
+    // disconnect bus signals
+    g_signal_handlers_disconnect_by_func(bus_, reinterpret_cast<gpointer>(OnConnected_),    this);
+    g_signal_handlers_disconnect_by_func(bus_, reinterpret_cast<gpointer>(OnDisconnected_), this);
+
+    DestroyContext();
+  }
+
+  void IBusIMEContext::Focus()
+  {
+    if (is_focused_)
+      return;
+    is_focused_ = true;
+
+    if (context_)
+      ibus_input_context_focus_in(context_);
+  }
+
+  void IBusIMEContext::Blur()
+  {
+    if (!is_focused_)
+      return;
+
+    is_focused_ = false;
+
+    if (context_)
+      ibus_input_context_focus_out(context_);
+  }
+
+  void IBusIMEContext::Reset()
+  {
+    if (context_)
+      ibus_input_context_reset(context_);
+  }
+
+  bool IBusIMEContext::FilterKeyEvent(const KeyEvent& event)
+  {
+    guint keyval = event.key_code(); // todo(jaytaoko): ui::GdkKeyCodeForWindowsKeyCode(event.key_code(), event.IsShiftDown() ^ event.IsCapsLockDown());
+
+    if (context_) {
+      guint modifiers = 0;
+
+      if (event.type() == EVENT_KEY_UP)
+        modifiers |= IBUS_RELEASE_MASK;
+
+      if (event.IsShiftDown())
+        modifiers |= IBUS_SHIFT_MASK;
+      if (event.IsControlDown())
+        modifiers |= IBUS_CONTROL_MASK;
+      if (event.IsAltDown())
+        modifiers |= IBUS_MOD1_MASK;
+      if (event.IsCapsLockDown())
+        modifiers |= IBUS_LOCK_MASK;
+
+      ibus_input_context_process_key_event_async(context_,
+        keyval, 0, modifiers,
+        -1,
+        NULL,
+        reinterpret_cast<GAsyncReadyCallback>(ProcessKeyEventDone),
+        new ProcessKeyEventData(this, event));
+      return true;
+    }
+
+    return false;
+  }
+
+  void IBusIMEContext::SetCursorLocation(const Rect& caret_rect)
+  {
+    if (context_ && caret_rect_ != caret_rect)
+    {
+      caret_rect_ = caret_rect;
+      ibus_input_context_set_cursor_location(context_,
+        caret_rect_.x,
+        caret_rect_.y,
+        caret_rect_.width,
+        caret_rect_.height);
+    }
+  }
+
+  void IBusIMEContext::SetSurrounding(const std::wstring& text, int cursor_pos)
+  {
+      // TODO(penghuang) support surrounding
+  }
+
+  void IBusIMEContext::CreateContext() {
+    nuxAssert(bus_ != NULL);
+    nuxAssert(ibus_bus_is_connected(bus_));
+
+    context_ = ibus_bus_create_input_context(bus_, "chrome");
+
+    // connect input context signals
+    g_signal_connect(context_, "commit-text",         G_CALLBACK(OnCommitText_),        this);
+    g_signal_connect(context_, "forward-key-event",   G_CALLBACK(OnForwardKeyEvent_),   this);
+    g_signal_connect(context_, "update-preedit-text", G_CALLBACK(OnUpdatePreeditText_), this);
+    g_signal_connect(context_, "show-preedit-text",   G_CALLBACK(OnShowPreeditText_),   this);
+    g_signal_connect(context_, "hide-preedit-text",   G_CALLBACK(OnHidePreeditText_),   this);
+    g_signal_connect(context_, "enabled",             G_CALLBACK(OnEnable_),            this);
+    g_signal_connect(context_, "disabled",            G_CALLBACK(OnDisable_),           this);
+    g_signal_connect(context_, "destroy",             G_CALLBACK(OnDestroy_),           this);
+
+    guint32 caps = IBUS_CAP_PREEDIT_TEXT |
+      IBUS_CAP_FOCUS |
+      IBUS_CAP_SURROUNDING_TEXT;
+    ibus_input_context_set_capabilities(context_, caps);
+
+    if (is_focused_)
+      ibus_input_context_focus_in(context_);
+
+    ibus_input_context_set_cursor_location(context_,
+      caret_rect_.x,
+      caret_rect_.y,
+      caret_rect_.width,
+      caret_rect_.height);
+  }
+
+  void IBusIMEContext::DestroyContext()
+  {
+    if (!context_)
+      return;
+
+    ibus_proxy_destroy(reinterpret_cast<IBusProxy *>(context_));
+
+    nuxAssert(!context_);
+  }
+
+  void IBusIMEContext::OnConnected(IBusBus *bus)
+  {
+    nuxDebugMsg("***IBusIMEContext::OnConnected***");
+
+    nuxAssert(bus_ == bus);
+    nuxAssert(ibus_bus_is_connected(bus_));
+
+    if (context_)
+      DestroyContext();
+    CreateContext();
+  }
+
+  void IBusIMEContext::OnDisconnected(IBusBus *bus)
+  {
+  }
+
+  void IBusIMEContext::OnCommitText(IBusInputContext *context, IBusText* text)
+  {
+    nuxAssert(context_ == context);
+    IMEContext::CommitText(ANSICHAR_TO_UNICHAR(text->text));
+  }
+
+  void IBusIMEContext::OnForwardKeyEvent(IBusInputContext *context, guint keyval, guint keycode, guint state)
+  {
+    nuxAssert(context_ == context);
+
+    int flags = 0;
+
+    if (state & IBUS_LOCK_MASK)
+      flags |= KEY_MODIFIER_CAPS_LOCK;
+    if (state & IBUS_CONTROL_MASK)
+      flags |= KEY_MODIFIER_CTRL;
+    if (state & IBUS_SHIFT_MASK)
+      flags |= KEY_MODIFIER_SHIFT;
+    if (state & IBUS_MOD1_MASK)
+      flags |= KEY_MODIFIER_ALT;
+
+    int mouse_state = 0;
+    if (state & IBUS_BUTTON1_MASK)
+      mouse_state |= MOUSE_BUTTON1;
+    if (state & IBUS_BUTTON2_MASK)
+      mouse_state |= MOUSE_BUTTON2;
+    if (state & IBUS_BUTTON3_MASK)
+      mouse_state |= MOUSE_BUTTON3;
+
+    ForwardKeyEvent(KeyEvent(state & IBUS_RELEASE_MASK ? EVENT_KEY_DOWN : EVENT_KEY_UP, keyval /* todo(jaytaoko): ui::WindowsKeyCodeForGdkKeyCode(keyval)*/, mouse_state, flags));
+  }
+
+  void IBusIMEContext::OnUpdatePreeditText(IBusInputContext *context, IBusText* text, guint cursor_pos, gboolean visible)
+  {
+    nuxAssert(context_ == context);
+    nuxAssert(IBUS_IS_TEXT(text));
+
+    if (visible)
+    {
+      CompositionAttributeList attributes;
+      IBusAttrList *attrs = text->attrs;
+      if (attrs)
+      {
+        int i = 0;
+        while (1)
+        {
+          IBusAttribute *attr = ibus_attr_list_get(attrs, i++);
+          if (!attr)
+            break;
+          if (attr->type == IBUS_ATTR_TYPE_UNDERLINE || attr->type == IBUS_ATTR_TYPE_BACKGROUND)
+          {
+            CompositionAttribute attribute(attr->start_index,
+              attr->end_index,
+              color::Black,
+              false);
+            if (attr->type == IBUS_ATTR_TYPE_BACKGROUND)
+            {
+              attribute.thick =  true;
+            } else if (attr->type == IBUS_ATTR_TYPE_UNDERLINE)
+            {
+              if (attr->value == IBUS_ATTR_UNDERLINE_DOUBLE)
+              {
+                attribute.thick = true;
+              } else if (attr->value == IBUS_ATTR_UNDERLINE_ERROR)
+              {
+                attribute.color = color::Red;
+              }
+            }
+            attributes.push_back(attribute);
+          }
+        }
+      }
+
+      SetComposition(ANSICHAR_TO_UNICHAR(text->text), attributes, cursor_pos);
+    }
+    else
+    {
+      EndComposition();
+    }
+  }
+
+  void IBusIMEContext::OnShowPreeditText(IBusInputContext *context)
+  {
+    nuxAssert(context_ == context);
+  }
+
+  void IBusIMEContext::OnHidePreeditText(IBusInputContext *context)
+  {
+    nuxAssert(context_ == context);
+
+    EndComposition();
+  }
+
+  void IBusIMEContext::OnEnable(IBusInputContext *context)
+  {
+    nuxAssert(context_ == context);
+  }
+
+  void IBusIMEContext::OnDisable(IBusInputContext *context)
+  {
+    nuxAssert(context_ == context);
+  }
+
+  void IBusIMEContext::OnDestroy(IBusInputContext *context)
+  {
+    nuxAssert(context_ == context);
+
+    g_object_unref(context_);
+    context_ = NULL;
+
+    EndComposition();
+  }
+
+  void IBusIMEContext::ProcessKeyEventDone(IBusInputContext *context, GAsyncResult* res, ProcessKeyEventData *data)
+  {
+      nuxAssert(data->context->context_ == context);
+
+      gboolean processed = FALSE;
+
+      if (!ibus_input_context_process_key_event_async_finish(context,
+        res,
+        &processed,
+        NULL))
+        processed = FALSE;
+
+      if (processed == FALSE)
+        data->context->ForwardKeyEvent(data->event);
+
+      delete data;
+  }
+
+  IMEContext* IMEContext::Create(View* view)
+  {
+    return new IBusIMEContext(view);
+  }
+
+}
