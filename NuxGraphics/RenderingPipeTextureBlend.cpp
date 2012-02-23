7288f3981a8739790dfa6aef55b80c154eb996de
Robert Carr <racarr@canonical.com>
2012-02-23 11:30:14 -0500

    Tweaktweak
    (bzr r571.3.16)

--- NuxGraphics/RenderingPipeTextureBlend.cpp
+++ NuxGraphics/RenderingPipeTextureBlend.cpp
@@ -398,7 +398,6 @@ ObjectPtr<IOpenGLShaderProgram> GraphicsEngine::GetBlendTexColorProgram (BlendMo
                      vec4 v = SampleTexture(TextureObject0, varyTexCoord0);      \n\
                      gl_FragColor.rgb = mix(v.rgb,%s(v.rgb,color0),BlendOpacity); \n\
                      gl_FragColor.a = 1.0; \n\
-                     gl_FragColor *= varyVertexColor; \n\
                      }";
 
     //                     gl_FragColor = %s(v*varyVertexColor, blendColor);                           
