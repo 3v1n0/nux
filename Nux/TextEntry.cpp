1e2d60a1d17095543b58748bfc2257f5d88abb85
Jay Taoko <jay.taoko@canonical.com>
2012-03-02 14:29:40 -0500

    * Switch the TextEntry to text input mode (TextEntry::text_input_mode_) following the operation TexTEntry::SetText, TexTEntry::EnterText, TexTEntry::DeleteText. TextEntry::text_input_mode_ is also set to true when a key is pressed in the TextEntry.
    (bzr r587.1.1)

--- Nux/TextEntry.cpp
+++ Nux/TextEntry.cpp
@@ -590,6 +590,8 @@ namespace nux
     selection_bound_ = 0;
     need_im_reset_ = true;
     //ResetImContext();
+
+    text_input_mode_ = true;
     QueueRefresh(true, true);
     text_changed.emit(this);
   }
@@ -1497,6 +1499,7 @@ namespace nux
     }
 
     ResetLayout();
+    text_input_mode_ = true;
     text_changed.emit(this);
   }
 
@@ -1528,6 +1531,7 @@ namespace nux
       selection_bound_ -= (end - start);
 
     ResetLayout();
+    text_input_mode_ = true;
     text_changed.emit(this);
   }
 
