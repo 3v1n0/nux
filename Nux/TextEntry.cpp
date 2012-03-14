b8c77e596d2a71f80df484f41a67f304689f5a9a
Koichi Akabe <vbkaisetsu@gmail.com>
2012-03-14 20:03:21 +0900

    check retval of FilterKeyEvent when pressed key is ENTER
    Fixes LP: #954901
    
    (bzr r592.1.1)

--- Nux/TextEntry.cpp
+++ Nux/TextEntry.cpp
@@ -341,7 +341,7 @@ namespace nux
     if (keysym == NUX_VK_TAB)
       return;
 
-    if (keysym == NUX_VK_ENTER || keysym == NUX_KP_ENTER)
+    if ((keysym == NUX_VK_ENTER || keysym == NUX_KP_ENTER) && !retval)
     {
       activated.emit();
       return;
