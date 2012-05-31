5b2d8945e24a21bdac2c62d08b8707544f76f2cd
Daniel d'Andrada <daniel.dandrada@canonical.com>
2012-05-31 18:51:25 -0300

    Tests for the gestures feature
    (bzr r637.2.8)

---
+++ tests/geis_mock.cpp
@@ -0,0 +1,482 @@
+/*
+ * Copyright 2012 Canonical Ltd.
+ *
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License, as
+ * published by the  Free Software Foundation; either version 2.1 or 3.0
+ * of the License.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranties of
+ * MERCHANTABILITY, SATISFACTORY QUALITY or FITNESS FOR A PARTICULAR
+ * PURPOSE.  See the applicable version of the GNU Lesser General Public
+ * License for more details.
+ *
+ * You should have received a copy of both the GNU Lesser General Public
+ * License along with this program. If not, see <http://www.gnu.org/licenses/>
+ *
+ * Authored by: Daniel d'Andrada <daniel.dandrada@canonical.com>
+ *
+ */
+
+#include <geis/geis.h>
+#include "geis_mock.h"
+
+#include <stdio.h>
+#include <string.h>
+#include <sys/eventfd.h>
+
+//#define DEBUG_PRINT printf("%s mock called!\n", __func__);
+#define DEBUG_PRINT
+
+/******** GeisAttributes helper class *********/
+
+GeisAttr GeisAttributes::GetByName(GeisString name)
+{
+  for (size_t i = 0; i < vector.size(); ++i)
+  {
+    GeisAttr attr = vector[i].get();
+    if (attr->name.compare(name) == 0)
+      return attr;
+  }
+  return nullptr;
+}
+
+GeisAttr GeisAttributes::GetByIndex(GeisSize index)
+{
+  if (vector.size() > index)
+  {
+    return vector[index].get();
+  }
+  else
+  {
+    return nullptr;
+  }
+}
+
+GeisSize GeisAttributes::Count()
+{
+  return vector.size();
+}
+
+void GeisAttributes::AddBoolean(const char *name, GeisBoolean value)
+{
+  std::unique_ptr<struct _GeisAttr> attr(new struct _GeisAttr);
+  attr->type = GEIS_ATTR_TYPE_BOOLEAN;
+  attr->name = name;
+  attr->b = value;
+  vector.push_back(std::move(attr));
+}
+
+void GeisAttributes::AddFloat(const char *name, GeisFloat value)
+{
+  std::unique_ptr<struct _GeisAttr> attr(new struct _GeisAttr);
+  attr->type = GEIS_ATTR_TYPE_FLOAT;
+  attr->name = name;
+  attr->f = value;
+  vector.push_back(std::move(attr));
+}
+
+void GeisAttributes::AddInteger(const char *name, GeisInteger value)
+{
+  std::unique_ptr<struct _GeisAttr> attr(new struct _GeisAttr);
+  attr->type = GEIS_ATTR_TYPE_INTEGER;
+  attr->name = name;
+  attr->i = value;
+  vector.push_back(std::move(attr));
+}
+
+void GeisAttributes::AddPointer(const char *name, void *value)
+{
+  std::unique_ptr<struct _GeisAttr> attr(new struct _GeisAttr);
+  attr->type = GEIS_ATTR_TYPE_POINTER;
+  attr->name = name;
+  attr->p    = value;
+  vector.push_back(std::move(attr));
+}
+
+/******* Geis  *******/
+
+Geis geis_new(GeisString init_arg_name, ...)
+{
+  DEBUG_PRINT
+  Geis geis = new struct _Geis;
+  geis->fd = eventfd(0, EFD_NONBLOCK);
+  return geis;
+}
+
+GeisStatus geis_delete(Geis geis)
+{
+  DEBUG_PRINT
+  close(geis->fd);
+  delete geis;
+  return GEIS_STATUS_SUCCESS;
+}
+
+GeisStatus geis_get_configuration(Geis geis,
+                                  GeisString configuration_item_name,
+                                  void *configuration_item_value)
+{
+  DEBUG_PRINT
+  if (strcmp(configuration_item_name, GEIS_CONFIGURATION_FD) == 0)
+  {
+    int *fd = static_cast<int*>(configuration_item_value);
+    *fd = geis->fd;
+    return GEIS_STATUS_SUCCESS;
+  }
+  else
+  {
+    return GEIS_STATUS_NOT_SUPPORTED;
+  }
+}
+
+GeisStatus geis_dispatch_events(Geis geis)
+{
+  DEBUG_PRINT
+  return GEIS_STATUS_SUCCESS;
+}
+
+GeisStatus geis_next_event(Geis geis, GeisEvent *event)
+{
+  DEBUG_PRINT
+
+  if (geis->pending_events.size() > 0)
+  {
+    *event = geis->pending_events.front();
+    geis->pending_events.pop_front();
+
+    if (geis->pending_events.size() > 0)
+      return GEIS_STATUS_CONTINUE;
+    else
+      return GEIS_STATUS_SUCCESS;
+  }
+  else
+  {
+    *event = nullptr;
+    return GEIS_STATUS_EMPTY;
+  }
+}
+
+GeisStatus geis_gesture_accept(Geis          geis,
+                               GeisGroup     group,
+                               GeisGestureId gesture_id)
+{
+  DEBUG_PRINT
+  return GEIS_STATUS_SUCCESS;
+}
+
+GeisStatus geis_gesture_reject(Geis          geis,
+                               GeisGroup     group,
+                               GeisGestureId gesture_id)
+{
+  DEBUG_PRINT
+  return GEIS_STATUS_SUCCESS;
+}
+
+/******* Attr  *******/
+
+GeisString geis_attr_name(GeisAttr attr)
+{
+  DEBUG_PRINT
+  return attr->name.c_str();
+}
+
+GeisAttrType geis_attr_type(GeisAttr attr)
+{
+  DEBUG_PRINT
+  return attr->type;
+}
+
+GeisBoolean geis_attr_value_to_boolean(GeisAttr attr)
+{
+  DEBUG_PRINT
+  return attr->b;
+}
+
+GeisFloat geis_attr_value_to_float(GeisAttr attr)
+{
+  DEBUG_PRINT
+  return attr->f;
+}
+
+GeisInteger geis_attr_value_to_integer(GeisAttr attr)
+{
+  DEBUG_PRINT
+  return attr->i;
+}
+
+GeisPointer geis_attr_value_to_pointer(GeisAttr attr)
+{
+  DEBUG_PRINT
+  return attr->p;
+}
+
+GeisString geis_attr_value_to_string(GeisAttr attr)
+{
+  DEBUG_PRINT
+  return attr->s.c_str();
+}
+
+/*********** Event  **************/
+
+void geis_event_delete(GeisEvent event)
+{
+  DEBUG_PRINT
+  delete event;
+}
+
+GeisEventType geis_event_type(GeisEvent event)
+{
+  DEBUG_PRINT
+  return event->type;
+}
+
+GeisSize geis_event_attr_count(GeisEvent event)
+{
+  DEBUG_PRINT
+  return event->attributes.Count();
+}
+
+GeisAttr geis_event_attr(GeisEvent event, GeisSize index)
+{
+  DEBUG_PRINT
+  return event->attributes.GetByIndex(index);
+}
+
+GeisAttr geis_event_attr_by_name(GeisEvent event, GeisString attr_name)
+{
+  DEBUG_PRINT
+  return event->attributes.GetByName(attr_name);
+}
+
+/******** Gesture Class   ***********/
+
+void geis_gesture_class_ref(GeisGestureClass gesture_class)
+{
+  DEBUG_PRINT
+}
+
+void geis_gesture_class_unref(GeisGestureClass gesture_class)
+{
+  DEBUG_PRINT
+}
+
+GeisString geis_gesture_class_name(GeisGestureClass gesture_class)
+{
+  DEBUG_PRINT
+  return const_cast<char *>(gesture_class->name.c_str());
+}
+
+GeisInteger geis_gesture_class_id(GeisGestureClass gesture_class)
+{
+  DEBUG_PRINT
+  return gesture_class->id;
+}
+
+GeisSize geis_gesture_class_attr_count(GeisGestureClass gesture_class)
+{
+  DEBUG_PRINT
+  return gesture_class->attributes.Count();
+}
+
+GeisAttr geis_gesture_class_attr(GeisGestureClass gesture_class, int index)
+{
+  DEBUG_PRINT
+  return gesture_class->attributes.GetByIndex(index);
+}
+
+/********* Device ***********/
+
+GeisInteger geis_device_id(GeisDevice device)
+{
+  DEBUG_PRINT
+  return device->id;
+}
+
+GeisAttr geis_device_attr_by_name(GeisDevice device, GeisString attr_name)
+{
+  DEBUG_PRINT
+  return device->attributes.GetByName(attr_name);
+}
+
+/********** Group Set **********/
+
+GeisSize geis_groupset_group_count(GeisGroupSet groupset)
+{
+  DEBUG_PRINT
+  return groupset->vector.size();
+}
+
+GeisGroup geis_groupset_group(GeisGroupSet groupset, GeisSize index)
+{
+  DEBUG_PRINT
+  return groupset->vector[index];
+}
+
+/****** Group ******/
+
+GeisSize geis_group_frame_count(GeisGroup group)
+{
+  DEBUG_PRINT
+  return group->vector.size();
+}
+
+GeisFrame geis_group_frame(GeisGroup group, GeisSize index)
+{
+  DEBUG_PRINT
+  return group->vector[index];
+}
+
+/***** Touch Set *****/
+
+GeisSize geis_touchset_touch_count(GeisTouchSet touchset)
+{
+  DEBUG_PRINT
+  return touchset->vector.size();
+}
+
+GeisTouch geis_touchset_touch(GeisTouchSet touchset, GeisSize index)
+{
+  DEBUG_PRINT
+  return touchset->vector[index];
+}
+
+/***** Touch ******/
+
+GeisTouchId geis_touch_id(GeisTouch touch)
+{
+  DEBUG_PRINT
+  return touch->id;
+}
+
+GeisSize geis_touch_attr_count(GeisTouch touch)
+{
+  DEBUG_PRINT
+  return touch->attributes.Count();
+}
+
+GeisAttr geis_touch_attr(GeisTouch touch, GeisSize index)
+{
+  DEBUG_PRINT
+  return touch->attributes.GetByIndex(index);
+}
+
+GeisAttr geis_touch_attr_by_name(GeisTouch touch, GeisString name)
+{
+  DEBUG_PRINT
+  return touch->attributes.GetByName(name);
+}
+
+/***** Frame *****/
+
+GeisGestureId geis_frame_id(GeisFrame frame)
+{
+  DEBUG_PRINT
+  return frame->id;
+}
+
+GeisBoolean geis_frame_is_class(GeisFrame        frame,
+                                GeisGestureClass gesture_class)
+{
+  DEBUG_PRINT
+  std::set<int>::iterator it = frame->class_ids.find(gesture_class->id);
+  if (it == frame->class_ids.end())
+    return GEIS_FALSE;
+  else
+    return GEIS_TRUE;
+}
+
+GeisSize geis_frame_attr_count(GeisFrame frame)
+{
+  DEBUG_PRINT
+  return frame->attributes.Count();
+}
+
+GeisAttr geis_frame_attr(GeisFrame frame, GeisSize index)
+{
+  DEBUG_PRINT
+  return frame->attributes.GetByIndex(index);
+}
+
+GeisAttr geis_frame_attr_by_name(GeisFrame frame, GeisString name)
+{
+  DEBUG_PRINT
+  return frame->attributes.GetByName(name);
+}
+
+/***** Subscription *****/
+
+GeisSubscription geis_subscription_new(Geis                  geis,
+                                       GeisString            name,
+                                       GeisSubscriptionFlags flags)
+{
+  DEBUG_PRINT
+  return new struct _GeisSubscription;
+}
+
+GeisStatus geis_subscription_delete(GeisSubscription subscription)
+{
+  DEBUG_PRINT
+  for (auto filter : subscription->filters)
+  {
+    delete filter;
+  }
+  delete subscription;
+  return GEIS_STATUS_SUCCESS;
+}
+
+GeisStatus geis_subscription_activate(GeisSubscription subscription)
+{
+  DEBUG_PRINT
+  return GEIS_STATUS_SUCCESS;
+}
+
+GeisStatus geis_subscription_deactivate(GeisSubscription subscription)
+{
+  DEBUG_PRINT
+  return GEIS_STATUS_SUCCESS;
+}
+
+GeisString geis_subscription_name(GeisSubscription subscription)
+{
+  DEBUG_PRINT
+  return const_cast<char *>(subscription->name.c_str());
+}
+
+GeisInteger geis_subscription_id(GeisSubscription subscription)
+{
+  DEBUG_PRINT
+  return subscription->id;
+}
+
+GeisStatus geis_subscription_add_filter(GeisSubscription subscription,
+                                        GeisFilter       filter)
+{
+  DEBUG_PRINT
+  subscription->filters.push_back(filter);
+  return GEIS_STATUS_SUCCESS;
+}
+
+GeisFilter geis_subscription_filter_by_name(GeisSubscription sub,
+                                            GeisString       name)
+{
+  DEBUG_PRINT
+  return nullptr;
+}
+
+GeisStatus geis_subscription_remove_filter(GeisSubscription subscription,
+                                          GeisFilter       filter)
+{
+  DEBUG_PRINT
+  std::vector<GeisFilter>::iterator it = subscription->filters.begin();
+  while (it != subscription->filters.end())
+  {
+    if (filter == *it)
+    {
+      subscription->filters.erase(it);
+      return GEIS_STATUS_SUCCESS;
+    }
+    ++it;
+  }
+  return GEIS_STATUS_BAD_ARGUMENT;
+}
--- tests/gtest-nux-windowcompositor.cpp
+++ tests/gtest-nux-windowcompositor.cpp
@@ -5,14 +5,111 @@
 #include <gmock/gmock.h>
 #include <boost/filesystem.hpp>
 #include <glib.h>
+#include <list>
 
 #include "Nux/Nux.h"
+#include "Nux/BaseWindow.h"
 #include "Nux/HLayout.h"
+#include "Nux/VLayout.h"
 #include "Nux/ProgramFramework/TestView.h"
+#ifdef NUX_GESTURES_SUPPORT
+#include "NuxGraphics/GestureEvent.h"
+#endif
 
 using namespace testing;
 
+
+#ifdef NUX_GESTURES_SUPPORT
+namespace nux {
+class FakeGestureEvent
+{
+  public:
+    nux::EventType type;
+
+    int gesture_id;
+    int gesture_classes;
+    bool is_direct_touch;
+    int timestamp;
+    nux::Point2D<float> focus;
+    nux::Point2D<float> delta;
+    float angle;
+    float angle_delta;
+    float angular_velocity;
+    int tap_duration;
+    nux::Point2D<float> velocity;
+    float radius;
+    float radius_delta;
+    float radial_velocity;
+    std::vector<nux::TouchPoint> touches;
+    bool is_construction_finished;
+
+    nux::GestureEvent &ToGestureEvent()
+    {
+      event_.type = type;
+
+      event_.gesture_id_ = gesture_id;
+      event_.gesture_classes_ = gesture_classes;
+      event_.is_direct_touch_ = is_direct_touch;
+      event_.timestamp_ = timestamp;
+      event_.focus_ = focus;
+      event_.delta_ = delta;
+      event_.angle_ = angle;
+      event_.angle_delta_ = angle_delta;
+      event_.angular_velocity_ = angular_velocity;
+      event_.tap_duration_ = tap_duration;
+      event_.velocity_ = velocity;
+      event_.radius_ = radius;
+      event_.radius_delta_ = radius_delta;
+      event_.radial_velocity_ = radial_velocity;
+      event_.touches_ = touches;
+      event_.is_construction_finished_ = is_construction_finished;
+
+      return event_;
+    }
+
+  private:
+    nux::GestureEvent event_;
+};
+} // namespace nux
+
+int g_gesture_event_accept_count;
+void nux::GestureEvent::Accept()
+{
+  ++g_gesture_event_accept_count;
+}
+
+int g_gesture_event_reject_count;
+void nux::GestureEvent::Reject()
+{
+  ++g_gesture_event_reject_count;
+}
+#endif // NUX_GESTURES_SUPPORT
+
 namespace {
+
+#ifdef NUX_GESTURES_SUPPORT
+class TestWindow : public nux::BaseWindow
+{
+  public:
+  virtual void GestureEvent(const nux::GestureEvent &event)
+  {
+    gesture_events_received.push_back(event);
+  }
+
+  std::list<nux::GestureEvent> gesture_events_received;
+};
+
+class TestInputArea : public nux::InputArea
+{
+  public:
+  virtual void GestureEvent(const nux::GestureEvent &event)
+  {
+    gesture_events_received.push_back(event);
+  }
+
+  std::list<nux::GestureEvent> gesture_events_received;
+};
+#endif
 
 TEST(TestWindowCompositor, TestSetKeyFocusArea)
 {
@@ -60,5 +157,213 @@ TEST(TestWindowCompositor, TestSetKeyFocusArea)
   delete wnd_thread;
 }
 
+#ifdef NUX_GESTURES_SUPPORT
+
+/*
+  We have two windows: one subscribed to 2-touches' pinch (A) and
+  another without any subscriptions (B).
+
+  Send events of a 2-touches' pinch within window A's boundaries.
+
+  Check that the gesture got accepted, that window A got the gesture
+  events and that window B didn't get anything.
+ */
+TEST(TestWindowCompositor, GestureEventsDelivery_1)
+{
+  nux::NuxInitialize(0);
+  nux::WindowThread *wnd_thread = nux::CreateNuxWindow("Nux Window", 500, 500,
+    nux::WINDOWSTYLE_NORMAL, NULL, false, NULL, NULL);
+  nux::WindowCompositor &wnd_compositor = wnd_thread->GetWindowCompositor();
+  nux::FakeGestureEvent fake_event;
+
+  TestWindow *target_window = new TestWindow;
+  target_window->SetBaseXY(10, 10);
+  target_window->SetBaseWidth(90);
+  target_window->SetBaseHeight(90);
+  target_window->ShowWindow(true);
+  target_window->CreateGesturesSubscription(nux::PINCH_GESTURE, 2);
+
+  TestWindow *innocent_window = new TestWindow;
+  innocent_window->SetBaseXY(300, 300);
+  innocent_window->SetBaseWidth(100);
+  innocent_window->SetBaseHeight(100);
+  innocent_window->ShowWindow(true);
+
+  g_gesture_event_accept_count = 0;
+  g_gesture_event_reject_count = 0;
+  fake_event.type = nux::EVENT_GESTURE_BEGIN;
+  fake_event.gesture_id = 0;
+  fake_event.gesture_classes = nux::PINCH_GESTURE | nux::ROTATE_GESTURE;
+  fake_event.is_direct_touch = false;
+  fake_event.focus.x = 30.0f; // hits target_window
+  fake_event.focus.y = 30.0f;
+  // in touch device's coordinate system (because it's not a direct device).
+  // Thus not used by WindowCompositor
+  fake_event.touches.push_back(nux::TouchPoint(0, 10.0f, 10.0f));
+  fake_event.touches.push_back(nux::TouchPoint(1, 20.0f, 20.0f));
+  fake_event.is_construction_finished = false;
+  wnd_compositor.ProcessEvent(fake_event.ToGestureEvent()); 
+
+  ASSERT_EQ(0, g_gesture_event_accept_count);
+  ASSERT_EQ(0, g_gesture_event_reject_count);
+  ASSERT_EQ(0, target_window->gesture_events_received.size());
+  ASSERT_EQ(0, innocent_window->gesture_events_received.size());
+
+  g_gesture_event_accept_count = 0;
+  g_gesture_event_reject_count = 0;
+  fake_event.type = nux::EVENT_GESTURE_UPDATE;
+  fake_event.focus.x += 2.0f;
+  fake_event.focus.y += 2.0f;
+  fake_event.touches.clear();
+  fake_event.touches.push_back(nux::TouchPoint(0, 11.0f, 11.0f));
+  fake_event.touches.push_back(nux::TouchPoint(1, 21.0f, 21.0f));
+  fake_event.is_construction_finished = true;
+  wnd_compositor.ProcessEvent(fake_event.ToGestureEvent()); 
+
+  ASSERT_EQ(1, g_gesture_event_accept_count);
+  ASSERT_EQ(0, g_gesture_event_reject_count);
+  ASSERT_EQ(2, target_window->gesture_events_received.size());
+  ASSERT_EQ(0, innocent_window->gesture_events_received.size());
+
+  target_window->Dispose();
+  innocent_window->Dispose();
+  delete wnd_thread;
+}
+
+/*
+  We have two windows: one subscribed to 2-touches' pinch (A) and
+  another without any subscriptions (B).
+
+  Send events of a 2-touches' pinch within window B's boundaries.
+
+  Check that the gesture got rejected and that no window got
+  any gesture event.
+*/
+TEST(TestWindowCompositor, GestureEventsDelivery_2)
+{
+  nux::NuxInitialize(0);
+  nux::WindowThread *wnd_thread = nux::CreateNuxWindow("Nux Window", 500, 500,
+    nux::WINDOWSTYLE_NORMAL, NULL, false, NULL, NULL);
+  nux::WindowCompositor &wnd_compositor = wnd_thread->GetWindowCompositor();
+  nux::FakeGestureEvent fake_event;
+
+  TestWindow *subscribed_window = new TestWindow;
+  subscribed_window->SetBaseXY(10, 10);
+  subscribed_window->SetBaseWidth(90);
+  subscribed_window->SetBaseHeight(90);
+  subscribed_window->ShowWindow(true);
+  subscribed_window->CreateGesturesSubscription(nux::PINCH_GESTURE, 2);
+
+  TestWindow *innocent_window = new TestWindow;
+  innocent_window->SetBaseXY(300, 300);
+  innocent_window->SetBaseWidth(100);
+  innocent_window->SetBaseHeight(100);
+  innocent_window->ShowWindow(true);
+
+  g_gesture_event_accept_count = 0;
+  g_gesture_event_reject_count = 0;
+  fake_event.type = nux::EVENT_GESTURE_BEGIN;
+  fake_event.gesture_id = 0;
+  fake_event.gesture_classes = nux::PINCH_GESTURE | nux::ROTATE_GESTURE;
+  fake_event.is_direct_touch = false;
+  fake_event.focus.x = 350.0f; // hits innocent_window
+  fake_event.focus.y = 350.0f;
+  // in touch device's coordinate system (because it's not a direct device).
+  // Thus not used by WindowCompositor
+  fake_event.touches.push_back(nux::TouchPoint(0, 10.0f, 10.0f));
+  fake_event.touches.push_back(nux::TouchPoint(1, 20.0f, 20.0f));
+  fake_event.is_construction_finished = false;
+  wnd_compositor.ProcessEvent(fake_event.ToGestureEvent()); 
+
+  ASSERT_EQ(0, g_gesture_event_accept_count);
+  ASSERT_EQ(1, g_gesture_event_reject_count);
+  ASSERT_EQ(0, subscribed_window->gesture_events_received.size());
+  ASSERT_EQ(0, innocent_window->gesture_events_received.size());
+
+  subscribed_window->Dispose();
+  innocent_window->Dispose();
+  delete wnd_thread;
+}
+
+/*
+  There are two input areas in main window's layout. They're both subscribed
+  to 2-touches' pinch gestures.
+  There's also a window on top that is subscribed to 2-touches' drag gestures.
+
+  Send events of a 2-touches' pinch within that window's boundaries.
+
+  Check that the gesture gets accepted and that only the input area behind that
+  window gets the gesture events.
+ */
+TEST(TestWindowCompositor, GestureEventsDelivery_3)
+{
+  nux::NuxInitialize(0);
+  nux::WindowThread *wnd_thread = nux::CreateNuxWindow("Nux Window", 500, 500,
+    nux::WINDOWSTYLE_NORMAL, NULL, false, NULL, NULL);
+  nux::WindowCompositor &wnd_compositor = wnd_thread->GetWindowCompositor();
+  nux::FakeGestureEvent fake_event;
+
+  TestWindow *window = new TestWindow;
+  window->SetBaseXY(10, 10);
+  window->SetBaseWidth(90);
+  window->SetBaseHeight(90);
+  window->ShowWindow(true);
+  window->CreateGesturesSubscription(nux::DRAG_GESTURE, 2);
+
+  TestInputArea *target_input_area = new TestInputArea;
+  target_input_area->CreateGesturesSubscription(nux::PINCH_GESTURE, 2);
+
+  TestInputArea *other_input_area = new TestInputArea;
+  other_input_area->CreateGesturesSubscription(nux::PINCH_GESTURE, 2);
+
+  nux::VLayout *root_layout = new nux::VLayout;
+  root_layout->AddView(target_input_area); // should have geometry (0, 0, 500, 250)
+  root_layout->AddView(other_input_area); // should have geometry (0, 250, 500, 250)
+  wnd_thread->SetLayout(root_layout);
+
+  g_gesture_event_accept_count = 0;
+  g_gesture_event_reject_count = 0;
+  fake_event.type = nux::EVENT_GESTURE_BEGIN;
+  fake_event.gesture_id = 0;
+  fake_event.gesture_classes = nux::PINCH_GESTURE | nux::ROTATE_GESTURE;
+  fake_event.is_direct_touch = false;
+  fake_event.focus.x = 30.0f; // hits window and target_input_area behind it
+  fake_event.focus.y = 30.0f;
+  // in touch device's coordinate system (because it's not a direct device).
+  // Thus not used by WindowCompositor
+  fake_event.touches.push_back(nux::TouchPoint(0, 10.0f, 10.0f));
+  fake_event.touches.push_back(nux::TouchPoint(1, 20.0f, 20.0f));
+  fake_event.is_construction_finished = false;
+  wnd_compositor.ProcessEvent(fake_event.ToGestureEvent()); 
+
+  // shouldn't be accepted right away because is_construction_finished == false
+  ASSERT_EQ(0, g_gesture_event_accept_count);
+  ASSERT_EQ(0, g_gesture_event_reject_count);
+  ASSERT_EQ(0, window->gesture_events_received.size());
+  ASSERT_EQ(0, target_input_area->gesture_events_received.size());
+  ASSERT_EQ(0, other_input_area->gesture_events_received.size());
+
+  g_gesture_event_accept_count = 0;
+  g_gesture_event_reject_count = 0;
+  fake_event.type = nux::EVENT_GESTURE_UPDATE;
+  fake_event.focus.x += 2.0f;
+  fake_event.focus.y += 2.0f;
+  fake_event.touches.clear();
+  fake_event.touches.push_back(nux::TouchPoint(0, 11.0f, 11.0f));
+  fake_event.touches.push_back(nux::TouchPoint(1, 21.0f, 21.0f));
+  fake_event.is_construction_finished = true;
+  wnd_compositor.ProcessEvent(fake_event.ToGestureEvent()); 
+
+  ASSERT_EQ(1, g_gesture_event_accept_count);
+  ASSERT_EQ(0, g_gesture_event_reject_count);
+  ASSERT_EQ(0, window->gesture_events_received.size());
+  ASSERT_EQ(2, target_input_area->gesture_events_received.size());
+  ASSERT_EQ(0, other_input_area->gesture_events_received.size());
+
+  window->Dispose();
+  delete wnd_thread;
+}
+
+#endif // NUX_GESTURES_SUPPORT
 
 }
