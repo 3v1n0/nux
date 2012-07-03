2971da1133fa44a93d1d7ed3b9b4e408da37f87b
Marco Trevisan (Treviño) <mail@3v1n0.net>
2012-07-03 03:35:22 +0200

    gtest-nux-textentry: added tests for invalid keys and Super/Alt keypress
    (bzr r624.1.13)

--- tests/gtest-nux-textentry.cpp
+++ tests/gtest-nux-textentry.cpp
@@ -1,18 +1,25 @@
-#include <string>
-#include <fstream>
-
-#include <iostream>
 #include <gmock/gmock.h>
-#include <boost/filesystem.hpp>
-#include <glib.h>
 
 #include "Nux/Nux.h"
 #include "Nux/TextEntry.h"
 
 
 using namespace testing;
+using namespace nux;
 
 namespace {
+
+class MockTextEntry : public nux::TextEntry
+{
+public:
+  MockTextEntry(const char* text) : nux::TextEntry(text)
+  {}
+
+  bool InspectKeyEvent(nux::Event const& event)
+  {
+    return nux::TextEntry::InspectKeyEvent(event);
+  }
+};
 
 class TestTextEntry : public Test
 {
@@ -23,14 +30,13 @@ public:
     wnd_thread.reset(nux::CreateNuxWindow("Nux Window", 300, 200,
                      nux::WINDOWSTYLE_NORMAL, NULL, false, NULL, NULL));
 
-    text_entry = new nux::TextEntry("");
+    text_entry = new MockTextEntry("");
     nux::GetWindowThread()->GetWindowCompositor().SetKeyFocusArea(text_entry.GetPointer());
   }
 
   std::unique_ptr<nux::WindowThread> wnd_thread;
-  nux::ObjectPtr<nux::TextEntry> text_entry;
+  nux::ObjectPtr<MockTextEntry> text_entry;
 };
-
 
 TEST_F(TestTextEntry, TestSetText)
 {
@@ -74,6 +80,57 @@ TEST_F(TestTextEntry, TestDeleteText)
   EXPECT_EQ(text_entry->GetText() == std::string(""), true);
 
   EXPECT_EQ(text_entry->IsInTextInputMode(), true);
+}
+
+#if defined(NUX_OS_LINUX)
+class TestEvent : public nux::Event
+{
+public:
+  TestEvent(nux::KeyModifier keymod, unsigned long keysym)
+  {
+    type = nux::NUX_KEYDOWN;
+    key_modifiers = keymod;
+    x11_keysym = keysym;
+  }
+
+  TestEvent(unsigned long keysym)
+  {
+    type = nux::NUX_KEYDOWN;
+    x11_keysym = keysym;
+  }
+};
+
+TEST_F(TestTextEntry, AltLinuxKeybindings)
+{
+  for (unsigned long keysym = 0; keysym < XK_VoidSymbol; ++keysym)
+  {
+    TestEvent event(KEY_MODIFIER_ALT, keysym);
+    EXPECT_FALSE(text_entry->InspectKeyEvent(event));
+  }
+}
+
+TEST_F(TestTextEntry, SuperLinuxKeybindings)
+{
+  for (unsigned long keysym = 0; keysym < XK_VoidSymbol; ++keysym)
+  {
+    TestEvent event(KEY_MODIFIER_SUPER, keysym);
+    EXPECT_FALSE(text_entry->InspectKeyEvent(event));
+  }
+}
+#endif
+
+TEST_F(TestTextEntry, InvalidKeys)
+{
+  std::vector<std::string> invalid_chars = {"", "", "", "", "",
+                                            "", "", "", "", "",
+                                            "", "", ""};
+  for (auto c : invalid_chars)
+  {
+    KeySym keysym = g_utf8_get_char(c.c_str());
+    text_entry->DeleteText(0, std::numeric_limits<int>::max());
+    text_entry->key_down.emit(NUX_KEYDOWN, keysym, 0, c.c_str(), 1);
+    EXPECT_EQ(text_entry->GetText(), "");
+  }
 }
 
 }
