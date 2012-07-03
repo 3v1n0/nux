4c0618cefbb8c4801de2ca1b3551a7c1e38942b1
Marco Trevisan (Treviño) <mail@3v1n0.net>
2012-07-03 18:23:16 +0200

    gtest-nux-textentry: fixed the tests when iBus is running
    (bzr r624.3.13)

--- tests/gtest-nux-textentry.cpp
+++ tests/gtest-nux-textentry.cpp
@@ -19,12 +19,16 @@
  *
  */
 
+#include "gtest-nux-utils.h"
+
 #include <gmock/gmock.h>
 
 #include "Nux/Nux.h"
 #include "Nux/TextEntry.h"
 #include "Nux/HLayout.h"
-
+#if defined(NUX_OS_LINUX)
+#include "Nux/InputMethodIBus.h"
+#endif
 
 using namespace testing;
 using namespace nux;
@@ -55,6 +59,21 @@ public:
   void SetCursor(int cursor)
   {
     TextEntry::SetCursor(cursor);
+  }
+
+  nux::IBusIMEContext* ime() const
+  {
+#if defined(NUX_OS_LINUX)
+    return ime_;
+#else
+    return nullptr;
+#endif
+  }
+
+  void WaitEvent()
+  {
+    if (im_running())
+      Utils::WaitForTimeoutMSec(100);
   }
 
   MOCK_METHOD0(CutClipboard, void());
@@ -161,7 +180,11 @@ TEST_F(TestTextEntry, AltLinuxKeybindings)
   for (unsigned long keysym = 0; keysym < XK_VoidSymbol; ++keysym)
   {
     TestEvent event(KEY_MODIFIER_ALT, keysym);
-    EXPECT_FALSE(text_entry->InspectKeyEvent(event));
+
+    if (text_entry->ime()->IsHotkeyEvent(event.type, event.GetKeySym(), event.key_modifiers))
+      EXPECT_TRUE(text_entry->InspectKeyEvent(event));
+    else
+      EXPECT_FALSE(text_entry->InspectKeyEvent(event));
   }
 }
 
@@ -170,7 +193,11 @@ TEST_F(TestTextEntry, SuperLinuxKeybindings)
   for (unsigned long keysym = 0; keysym < XK_VoidSymbol; ++keysym)
   {
     TestEvent event(KEY_MODIFIER_SUPER, keysym);
-    EXPECT_FALSE(text_entry->InspectKeyEvent(event));
+
+    if (text_entry->ime()->IsHotkeyEvent(event.type, event.GetKeySym(), event.key_modifiers))
+      EXPECT_TRUE(text_entry->InspectKeyEvent(event));
+    else
+      EXPECT_FALSE(text_entry->InspectKeyEvent(event));
   }
 }
 #endif
@@ -185,6 +212,7 @@ TEST_F(TestTextEntry, InvalidKeys)
     unsigned int keysym = g_utf8_get_char(c.c_str());
     text_entry->DeleteText(0, std::numeric_limits<int>::max());
     text_entry->key_down.emit(NUX_KEYDOWN, keysym, 0, c.c_str(), 1);
+    text_entry->WaitEvent();
     EXPECT_EQ(text_entry->GetText(), "");
   }
 }
@@ -194,6 +222,7 @@ TEST_F(TestTextEntry, CopyCtrlC)
   EXPECT_CALL(*text_entry, CopyClipboard());
   TestEvent event(KEY_MODIFIER_CTRL, NUX_VK_c);
   GetWindowCompositor().ProcessEvent(event);
+  text_entry->WaitEvent();
 }
 
 TEST_F(TestTextEntry, CopyCtrlIns)
@@ -201,6 +230,7 @@ TEST_F(TestTextEntry, CopyCtrlIns)
   EXPECT_CALL(*text_entry, CopyClipboard());
   TestEvent event(KEY_MODIFIER_CTRL, NUX_VK_INSERT);
   GetWindowCompositor().ProcessEvent(event);
+  text_entry->WaitEvent();
 }
 
 TEST_F(TestTextEntry, PasteCtrlV)
@@ -208,6 +238,7 @@ TEST_F(TestTextEntry, PasteCtrlV)
   EXPECT_CALL(*text_entry, PasteClipboard());
   TestEvent event(KEY_MODIFIER_CTRL, NUX_VK_v);
   GetWindowCompositor().ProcessEvent(event);
+  text_entry->WaitEvent();
 }
 
 TEST_F(TestTextEntry, PasteShiftIns)
@@ -215,6 +246,7 @@ TEST_F(TestTextEntry, PasteShiftIns)
   EXPECT_CALL(*text_entry, PasteClipboard());
   TestEvent event(KEY_MODIFIER_SHIFT, NUX_VK_INSERT);
   GetWindowCompositor().ProcessEvent(event);
+  text_entry->WaitEvent();
 }
 
 #if defined(NUX_OS_LINUX)
@@ -230,6 +262,7 @@ TEST_F(TestTextEntry, CutCtrlX)
   EXPECT_CALL(*text_entry, CutClipboard());
   TestEvent event(KEY_MODIFIER_CTRL, NUX_VK_x);
   GetWindowCompositor().ProcessEvent(event);
+  text_entry->WaitEvent();
 }
 
 TEST_F(TestTextEntry, CutShiftDel)
@@ -237,6 +270,7 @@ TEST_F(TestTextEntry, CutShiftDel)
   EXPECT_CALL(*text_entry, CutClipboard());
   TestEvent event(KEY_MODIFIER_SHIFT, NUX_VK_DELETE);
   GetWindowCompositor().ProcessEvent(event);
+  text_entry->WaitEvent();
 }
 
 TEST_F(TestTextEntry, CtrlA)
@@ -250,6 +284,7 @@ TEST_F(TestTextEntry, CtrlA)
   ASSERT_EQ(start, test_str.length());
 
   GetWindowCompositor().ProcessEvent(selectall);
+  text_entry->WaitEvent();
   EXPECT_TRUE(text_entry->GetSelectionBounds(&start, &end));
   EXPECT_EQ(start, 0);
   EXPECT_EQ(end, test_str.length());
@@ -265,18 +300,22 @@ TEST_F(TestTextEntry, MoveKeys)
 
   TestEvent right(NUX_VK_RIGHT);
   GetWindowCompositor().ProcessEvent(right);
+  text_entry->WaitEvent();
   EXPECT_EQ(text_entry->GetCursor(), 1);
 
   TestEvent end(NUX_VK_END);
   GetWindowCompositor().ProcessEvent(end);
+  text_entry->WaitEvent();
   EXPECT_EQ(text_entry->GetCursor(), test_str.length());
 
   TestEvent left(NUX_VK_LEFT);
   GetWindowCompositor().ProcessEvent(left);
+  text_entry->WaitEvent();
   EXPECT_EQ(text_entry->GetCursor(), 2);
 
   TestEvent home(NUX_VK_HOME);
   GetWindowCompositor().ProcessEvent(home);
+  text_entry->WaitEvent();
   EXPECT_EQ(text_entry->GetCursor(), 0);
 }
 
@@ -290,18 +329,22 @@ TEST_F(TestTextEntry, CtrlMoveKeys)
 
   TestEvent right(KEY_MODIFIER_CTRL, NUX_VK_RIGHT);
   GetWindowCompositor().ProcessEvent(right);
+  text_entry->WaitEvent();
   EXPECT_EQ(text_entry->GetCursor(), 3);
 
   TestEvent left(KEY_MODIFIER_CTRL, NUX_VK_LEFT);
   GetWindowCompositor().ProcessEvent(left);
+  text_entry->WaitEvent();
   EXPECT_EQ(text_entry->GetCursor(), 0);
 
   TestEvent end(KEY_MODIFIER_CTRL, NUX_VK_END);
   GetWindowCompositor().ProcessEvent(end);
+  text_entry->WaitEvent();
   EXPECT_EQ(text_entry->GetCursor(), test_str.length());
 
   TestEvent home(KEY_MODIFIER_CTRL, NUX_VK_HOME);
   GetWindowCompositor().ProcessEvent(home);
+  text_entry->WaitEvent();
   EXPECT_EQ(text_entry->GetCursor(), 0);
 }
 
@@ -313,11 +356,13 @@ TEST_F(TestTextEntry, DeleteKeys)
 
   TestEvent del(NUX_VK_DELETE);
   GetWindowCompositor().ProcessEvent(del);
+  text_entry->WaitEvent();
   EXPECT_EQ(text_entry->GetText(), "ux");
 
   text_entry->SetCursor(std::string(text_entry->GetText()).length());
   TestEvent backspace(NUX_VK_BACKSPACE);
   GetWindowCompositor().ProcessEvent(backspace);
+  text_entry->WaitEvent();
   EXPECT_EQ(text_entry->GetText(), "u");
 }
 
@@ -329,11 +374,13 @@ TEST_F(TestTextEntry, CtrlDeleteKeys)
 
   TestEvent del(KEY_MODIFIER_CTRL, NUX_VK_DELETE);
   GetWindowCompositor().ProcessEvent(del);
+  text_entry->WaitEvent();
   EXPECT_EQ(text_entry->GetText(), " Text Entry");
 
   text_entry->SetCursor(std::string(text_entry->GetText()).length());
   TestEvent backspace(KEY_MODIFIER_CTRL, NUX_VK_BACKSPACE);
   GetWindowCompositor().ProcessEvent(backspace);
+  text_entry->WaitEvent();
   EXPECT_EQ(text_entry->GetText(), " Text ");
 }
 }
