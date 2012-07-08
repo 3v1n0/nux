dd032713c4edb6818af4115e1f11063cab963468
Marco Trevisan (Treviño) <mail@3v1n0.net>
2012-07-09 01:40:46 +0200

    compose_key_list_generator.py: generate an array of keysyms vectors...
    (bzr r624.3.36)

--- tools/compose_key_list_generator.py
+++ tools/compose_key_list_generator.py
@@ -15,40 +15,31 @@
 import os, sys, re, codecs
 from gi.repository import Gdk
 
-DEFAULT_OUTPUT = "/tmp/parsed_keys.c"
+DEFAULT_OUTPUT = "/tmp/parsed_keys.cpp"
 input_file = sys.argv[1]
 output_file = sys.argv[2] if len(sys.argv) > 2 else DEFAULT_OUTPUT
 
 keys_match = re.compile("(?P<keybinding><[^:]+)[\s]*:[\s]*\"(\\\)?(?P<char>.*)\"[\s]*(?P<charcode>[^\s]+)?[\s]*#[\s]*(?P<desc>.+)")
+
 unicode_keys = re.compile("^U[0-9A-Fa-f]{4,6}$");
+blacklisted_keys = ["EZH", "ezh", "dead_double_grave", "dead_inverted_breve",
+                    "dead_greek", "greaterthanequal", "lessthanequal",
+                    "underbar", "rightarrow", "leftarrow"]
 
-combinations = {u"": ["ubuntu", "ucircle"]}
+combinations = {u"": ["ubuntu", "circle"]}
 descriptions = {u"": "UBUNTU CIRCLE"}
-
-dead_to_char_map = { "dead_grave": 'grave',
-                     "dead_acute": 'acute',
-                     "dead_circumflex": 'asciicircum',
-                     "dead_tilde": 'asciitilde',
-                     "dead_macron": 'macron',
-                     "dead_breve": 'breve',
-                     "dead_abovedot": 'abovedot',
-                     "dead_diaeresis": 'diaeresis',
-                     "dead_abovering": 'degree',
-                     "dead_doubleacute": 'doubleacute',
-                     "dead_caron": 'caron',
-                     "dead_cedilla": 'cedilla',
-                     "dead_ogonek": 'ogonek',
-                     "dead_stroke": 'slash',
-                     "dead_iota": 'Greek_iota',
-                     "dead_belowcomma": "comma",
-                     "dead_currency": "currency"
-                      }
 
 
 def key_list_has_invalid_keys(keys):
-    for k in keys:
-        if unicode_keys.match(k):
-            return True
+    if len([k for k in keys if unicode_keys.match(k)]):
+        return True
+
+    if 0 in [Gdk.keyval_from_name(k) for k in keys]:
+        return True
+
+    if len([k for k in keys if k in blacklisted_keys]):
+        return True
+
     return False
 
 
@@ -60,18 +51,21 @@ def escape_char(char):
     return char
 
 
-def make_c_output(output_file):
+def make_cpp_output(output_file):
     keycode = """/* This file has been automatically generated using the
  * compose_key_list_generator.py script.
  */
-#include <stdio.h>
+#include <vector>
+#include <X11/Xutils.h>
 
-/* This compose array lists all the characters needed to get each result, that
- * is listed after the special string "::"
- */
-static const char* compose_map[] = {
-%s
+struct ComposeSequence
+{
+  std::vector<KeySym> symbols;
+  const char* result;
 };
+
+static const ComposeSequence NUX_COMPOSE_SEQUENCIES[] = {
+%s};
 """
     keyarray = ""
 
@@ -79,15 +73,13 @@ static const char* compose_map[] = {
         if not len(combinations[c]):
             continue
 
-        for k in combinations[c]:
-            k = escape_char(k)
-
-            keyarray += "\"%s\", " % k
-
-        keyarray += "\"::\", \"%s\"" % escape_char(c)
-        keyarray += ", // %s\n" % descriptions[c]
-
-    keyarray += "\"\\0\" //NULL so we know when to end"
+        first = True
+        for combo in combinations[c]:
+            # Add 'XK_' prefix to key names
+            combo = ["XK_" + k for k in combo]
+            desc = " // " + descriptions[c] if first else ""
+            keyarray += "  {{%s}, \"%s\"},%s\n" % (", ".join(combo), escape_char(c), desc)
+            first = False
 
     output = keycode % (keyarray)
 
@@ -102,6 +94,8 @@ if not os.path.exists(input_file):
 
 print "Parsing "+input_file+", saving it to "+output_file+"..."
 composefile = codecs.open(input_file, 'r', 'utf-8')
+
+invalid = []
 
 for line in composefile:
     parts = re.match(keys_match, line)
@@ -122,37 +116,12 @@ for line in composefile:
     if key_list_has_invalid_keys(keys):
         continue
 
-    # Ignore Multi_key
-    if keys[0] == "Multi_key":
-        keys.pop(0)
-
-    # Convert dead keys to the relative key character
-    old_keys = keys
-    keys = [dead_to_char_map[k] if k in dead_to_char_map else k for k in keys]
-    has_dead_key = (old_keys != keys)
-
-    # Convert keys to KeySym values and then to the corresponding unicode char
-    keys = [unichr(Gdk.keyval_to_unicode(Gdk.keyval_from_name(k))) for k in keys]
-
-    # Ignoring keys where we have invalid values
-    if (u'\x00' in keys):
-        continue
-
-    # Join the chars to get the keys joined together
-    keys = u"".join(keys)
-
-    # If the sequence is already known, we remove it from other combinations
-    # or we ignore it depending if this was a dead key or not
-    ignore_key = False
-    for c in combinations.values():
-        if keys in c:
-            # Give priority to the non-dead key combo
-            if has_dead_key:
-                ignore_key = True
-            else:
-                c.remove(keys)
-
-    if ignore_key:
+    # Ignoring unknown keys
+    if 0 in [Gdk.keyval_from_name(k) for k in keys]:
+        for k in keys:
+            if Gdk.keyval_from_name(k) == 0 and k not in invalid:
+                invalid.append(k)
+                print k
         continue
 
     if not char in combinations:
@@ -165,5 +134,4 @@ for line in composefile:
         descriptions[char] = desc
 
 composefile.close();
-make_c_output(output_file)
-
+make_cpp_output(output_file)
