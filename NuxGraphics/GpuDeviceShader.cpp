3b864c12a1b3905411bacf5d31786d98c3c1b830
Eleni Maria Stea <elene.mst@gmail.com>
2012-09-14 14:10:51 +0300

    refactored: CreateVertexShader CreatePixelShader CreateAsmShaderProgram CreateAsmVertexShader CreateAsmPixelShader CreateCgVertexShader CreateCgPixelShader to use Adopt instead of private functions + UnReference
    (bzr r653.2.11)

--- NuxGraphics/GpuDeviceShader.cpp
+++ NuxGraphics/GpuDeviceShader.cpp
@@ -42,134 +42,70 @@ namespace nux
 		return ptr;
 	}
 
-  /*ObjectPtr<IOpenGLShaderProgram> GpuDevice::CreateShaderProgram()
-  {
-    IOpenGLShaderProgram *ptr;
-    CreateShaderProgram((IOpenGLShaderProgram **) &ptr);
-    ObjectPtr<IOpenGLShaderProgram> h = ObjectPtr<IOpenGLShaderProgram> (ptr);
-    ptr->UnReference();
-    return h;
-  }
+	ObjectPtr<IOpenGLVertexShader> GpuDevice::CreateVertexShader()
+	{
+		IOpenGLVertexShader *vsdr = new IOpenGLVertexShader();
+		ObjectPtr<IOpenGLVertexShader> ptr;
+		ptr.Adopt(vsdr);
 
-  int GpuDevice::CreateShaderProgram(IOpenGLShaderProgram **ppShaderProgram)
-  {
-    *ppShaderProgram = new IOpenGLShaderProgram();
+		return ptr;
+	}
 
-    return OGL_OK;
-  }*/
+	ObjectPtr<IOpenGLPixelShader> GpuDevice::CreatePixelShader()
+	{
+		IOpenGLPixelShader *fsdr = new IOpenGLPixelShader();
+		ObjectPtr<IOpenGLPixelShader> ptr;
+		ptr.Adopt(fsdr);
 
-  ObjectPtr<IOpenGLVertexShader> GpuDevice::CreateVertexShader()
-  {
-    IOpenGLVertexShader *ptr;
-    CreateVertexShader((IOpenGLVertexShader **) &ptr);
-    ObjectPtr<IOpenGLVertexShader> h = ObjectPtr<IOpenGLVertexShader> (ptr);
-    ptr->UnReference();
-    return h;
-  }
+		return ptr;
+	}
 
-  int GpuDevice::CreateVertexShader(IOpenGLVertexShader **ppVertexShader)
-  {
-    *ppVertexShader = new IOpenGLVertexShader();
+	ObjectPtr<IOpenGLAsmShaderProgram> GpuDevice::CreateAsmShaderProgram()
+	{
+		IOpenGLAsmShaderProgram *asprog = new IOpenGLAsmShaderProgram();
+		ObjectPtr<IOpenGLAsmShaderProgram> ptr;
+		ptr.Adopt(asprog);
 
-    return OGL_OK;
-  }
+		return ptr;
+	}
 
-  ObjectPtr<IOpenGLPixelShader> GpuDevice::CreatePixelShader()
-  {
-    IOpenGLPixelShader *ptr;
-    CreatePixelShader((IOpenGLPixelShader **) &ptr);
-    ObjectPtr<IOpenGLPixelShader> h = ObjectPtr<IOpenGLPixelShader> (ptr);
-    ptr->UnReference();
-    return h;
-  }
+	ObjectPtr<IOpenGLAsmVertexShader> GpuDevice::CreateAsmVertexShader()
+	{
+		IOpenGLAsmVertexShader *avsdr = new IOpenGLAsmVertexShader();
+		ObjectPtr<IOpenGLAsmVertexShader> ptr;
+		ptr.Adopt(avsdr);
 
-  int GpuDevice::CreatePixelShader(IOpenGLPixelShader **ppPixelShader)
-  {
-    *ppPixelShader = new IOpenGLPixelShader();
+		return ptr;
+	}
 
-    return OGL_OK;
-  }
+	ObjectPtr<IOpenGLAsmPixelShader> GpuDevice::CreateAsmPixelShader()
+	{
+		IOpenGLAsmPixelShader* afsdr = new IOpenGLAsmPixelShader();
+		ObjectPtr<IOpenGLAsmPixelShader> ptr;
+		ptr.Adopt(afsdr);
 
-  ObjectPtr<IOpenGLAsmShaderProgram> GpuDevice::CreateAsmShaderProgram()
-  {
-    IOpenGLAsmShaderProgram *ptr;
-    CreateAsmShaderProgram((IOpenGLAsmShaderProgram **) &ptr);
-    ObjectPtr<IOpenGLAsmShaderProgram> h = ObjectPtr<IOpenGLAsmShaderProgram> (ptr);
-    ptr->UnReference();
-    return h;
-  }
-
-  int GpuDevice::CreateAsmShaderProgram(IOpenGLAsmShaderProgram **ppAsmShaderProgram)
-  {
-    *ppAsmShaderProgram = new IOpenGLAsmShaderProgram();
-
-    return OGL_OK;
-  }
-
-  ObjectPtr<IOpenGLAsmVertexShader> GpuDevice::CreateAsmVertexShader()
-  {
-    IOpenGLAsmVertexShader *ptr;
-    CreateAsmVertexShader((IOpenGLAsmVertexShader **) &ptr);
-    ObjectPtr<IOpenGLAsmVertexShader> h = ObjectPtr<IOpenGLAsmVertexShader> (ptr);
-    ptr->UnReference();
-    return h;
-  }
-
-  int GpuDevice::CreateAsmVertexShader(IOpenGLAsmVertexShader **ppAsmVertexShader)
-  {
-    *ppAsmVertexShader = new IOpenGLAsmVertexShader();
-
-    return OGL_OK;
-  }
-
-  ObjectPtr<IOpenGLAsmPixelShader> GpuDevice::CreateAsmPixelShader()
-  {
-    IOpenGLAsmPixelShader *ptr;
-    CreateAsmPixelShader((IOpenGLAsmPixelShader **) &ptr);
-    ObjectPtr<IOpenGLAsmPixelShader> h = ObjectPtr<IOpenGLAsmPixelShader> (ptr);
-    ptr->UnReference();
-    return h;
-  }
-
-  int GpuDevice::CreateAsmPixelShader(IOpenGLAsmPixelShader **ppAsmPixelShader)
-  {
-    *ppAsmPixelShader = new IOpenGLAsmPixelShader();
-
-    return OGL_OK;
-  }
+		return ptr;
+	}
 
 #if (NUX_ENABLE_CG_SHADERS)
 
-  ObjectPtr<ICgVertexShader> GpuDevice::CreateCGVertexShader()
-  {
-    ICgVertexShader *ptr;
-    CreateCGVertexShader((ICgVertexShader **) &ptr);
-    ObjectPtr<ICgVertexShader> h = ptr;
-    ptr->UnReference();
-    return h;
-  }
+	ObjectPtr<ICgVertexShader> GpuDevice::CreateCGVertexShader()
+	{
+		ICgVertexShader* cvsdr = new ICgVertexShader();
+		ObjectPtr<ICgVertexShader> ptr;
+		ptr.Adopt(cvsdr);
 
-  int GpuDevice::CreateCGVertexShader(ICgVertexShader **ppCgVertexShader)
-  {
-    *ppCgVertexShader = new ICgVertexShader();
+		return ptr;
+	}
 
-    return OGL_OK;
-  }
+	ObjectPtr<ICgPixelShader> GpuDevice::CreateCGPixelShader()
+	{
+		ICgPixelShader *cfsdr = new ICgPixelShader();
+		ObjectPtr<ICgPixelShader> ptr;
+		ptr.Adopt(cfsdr);
 
-  ObjectPtr<ICgPixelShader> GpuDevice::CreateCGPixelShader()
-  {
-    ICgPixelShader *ptr;
-    CreateCGPixelShader((ICgPixelShader **) &ptr);
-    ObjectPtr<ICgPixelShader> h = ptr;
-    ptr->UnReference();
-    return h;
-  }
+		return ptr;
+	}
 
-  int GpuDevice::CreateCGPixelShader(ICgPixelShader **ppCgPixelShader)
-  {
-    *ppCgPixelShader = new ICgPixelShader();
-
-    return OGL_OK;
-  }
 #endif
 }
