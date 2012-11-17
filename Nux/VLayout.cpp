31ade212bcb4569a165f7d812f46f42b06375e6c
MC Return <mc.return@gmx.net>
2012-11-17 15:02:43 +0100

    Reduced the scope of the variables bool larger_width, smaller_width, larger_height and smaller_height
    (bzr r708.5.2)

--- Nux/VLayout.cpp
+++ Nux/VLayout.cpp
@@ -363,10 +363,6 @@ namespace nux
         if (!(*it)->IsVisible())
           continue;
 
-        bool larger_height  = false;
-        bool smaller_height = false;
-        bool smaller_width  = false;
-        bool larger_width   = false;
         int ret = 0;
         
         if (((*it)->IsLayout() || (*it)->IsView()) /*&& ((*it)->IsLayoutDone() == false)*/ /*&& ((*it)->GetScaleFactor() != 0)*/)
@@ -375,10 +371,10 @@ namespace nux
           ret = (*it)->ComputeContentSize();
           Geometry post_geo = (*it)->GetGeometry();
 
-          larger_width    = (pre_geo.width < post_geo.width)    ? true : false;
-          smaller_width   = (pre_geo.width > post_geo.width)    ? true : false;
-          larger_height   = (pre_geo.height < post_geo.height)  ? true : false;
-          smaller_height  = (pre_geo.height > post_geo.height)  ? true : false;
+          bool larger_width    = (pre_geo.width < post_geo.width)    ? true : false;
+          bool smaller_width   = (pre_geo.width > post_geo.width)    ? true : false;
+          bool larger_height   = (pre_geo.height < post_geo.height)  ? true : false;
+          bool smaller_height  = (pre_geo.height > post_geo.height)  ? true : false;
 
           if ((larger_height || smaller_height) && ((*it)->IsLayoutDone() == false))
           {
